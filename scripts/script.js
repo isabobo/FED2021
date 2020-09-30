4932ff34c793ee211a644c3c58d584ace0f57329
isabobo <60424638+isabobo@users.noreply.github.com>
2020-09-30 23:25:22 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-// JavaScript Document
+var button = document.querySelector('#deKnop');
+var list = document.querySelector('#jsForm')
+var cancel = document.querySelector('#cancelKnop');
+
+button.addEventListener('click', formPopup);
+
+function formPopup() {
+  list.classList.toggle("show");
+}
+
+cancel.addEventListener('click', formHide);
+
+function formHide() {
+  list.classList.toggle("hide");
+}
